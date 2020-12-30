af57f03dffd502a644120b1f50d2e2855f186524
Anuj Mathur <q3technologies@hotmail.com>
2020-12-30 20:10:34 +0530

    fourth commit

--- WEBAPICORETESTNEW/Controllers/AuthenticationController.cs
+++ WEBAPICORETESTNEW/Controllers/AuthenticationController.cs
@@ -120,6 +120,43 @@ namespace WEBAPICORETESTNEW.Controllers
             }
             return Unauthorized();
         }
+        [HttpPost]
+        [Route("LoginAdmin")]
+        public async Task<IActionResult> LoginAdmin([FromBody] LoginModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.UserName);
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                };
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(15),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                         );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo,
+                    User = user.UserName,
+                    Role = userRoles
+
+                });
+            }
+            return Unauthorized();
+        }
+
     }
 }
 
